71f03b1b152b366354b8db5991a7e7f4ec76a22e
Douglas José <douglasjose@gmail.com>
2015-06-11 23:46:45 -0400

    Open sourced under Apache 2.0 License.

--- src/test/java/com/douglasjose/tech/PlainJdbcTest.java
+++ src/test/java/com/douglasjose/tech/PlainJdbcTest.java
@@ -1,5 +1,21 @@
 package com.douglasjose.tech;
 
+/*
+Copyright 2015 Douglas José (douglasjose@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
